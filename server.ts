fe73a0984a3ade552572a9b37dbf9a18fc2698b9
Ivo Toby <ivo.toby@contentful.com>
2025-02-25 20:23:41 +0100

    fix: Update server static file paths to correctly serve React build files

--- server.ts
+++ server.ts
@@ -56,9 +56,9 @@ app.get('/todos', (req, res) => {
 });
 
 // Serve static files for the React frontend from the build directory:
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
